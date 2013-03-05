a525f4d8eef4261e4aeb474743facc69ac962958
Chris Bartley <bartley@cmu.edu>
2013-03-05 17:40:39 -0500

    Changed TextFileDataSampleStore to CsvDataSampleStore, now writes points out to a CSV file.

---
+++ java/code/airbot/src/org/bodytrack/airbot/CsvDataSampleStore.java
@@ -0,0 +1,110 @@
+package org.bodytrack.airbot;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * <p>
+ * <code>CsvDataSampleStore</code> handles storage {@link AirBot.DataSample data samples}, storing them in a text file
+ * as records with comma-delimited values.
+ * Does not support retrieval.
+ * </p>
+ *
+ * @author Chris Bartley (bartley@cmu.edu)
+ */
+public final class CsvDataSampleStore implements DataSampleStore
+   {
+   private static final Logger LOG = Logger.getLogger(CsvDataSampleStore.class);
+   private static final String COMMA = ",";
+
+   @NotNull
+   private BufferedWriter writer;
+
+   public CsvDataSampleStore(@NotNull final AirBotConfig airBotConfig)
+      {
+      final File dataFileDirectory = AirBotUploaderConstants.FilePaths.getDeviceDataDirectory(airBotConfig);
+      final File dataFile = new File(dataFileDirectory, "data_samples.csv");
+      final boolean doesFileAlreadyExist = dataFile.exists();
+      try
+         {
+         writer = new BufferedWriter(new FileWriter(dataFile, true));
+         if (!doesFileAlreadyExist)
+            {
+            write("sample_timestamp_utc_secs,particle_count,temperature,humidity,download_timestamp_utc_millis");
+            }
+         }
+      catch (FileNotFoundException e)
+         {
+         LOG.error("CsvDataSampleStore.CsvDataSampleStore(): FileNotFoundException while trying to create the BufferedWriter", e);
+         System.exit(1);
+         }
+      catch (IOException e)
+         {
+         LOG.error("CsvDataSampleStore.CsvDataSampleStore(): IOException while trying to create the BufferedWriter", e);
+         System.exit(1);
+         }
+      }
+
+   @Override
+   public boolean save(@NotNull final AirBot.DataSample dataSample)
+      {
+      LOG.debug("CsvDataSampleStore.save(): saving sample " + dataSample.getSampleTime());
+      try
+         {
+         write(dataSample);
+         }
+      catch (IOException e)
+         {
+         LOG.error("CsvDataSampleStore.save(): IOException while trying to write to the file", e);
+         }
+
+      return true;
+      }
+
+   private void write(final AirBot.DataSample dataSample) throws IOException
+      {
+      final StringBuilder s = new StringBuilder();
+      s.append(dataSample.getSampleTime());
+      s.append(COMMA);
+      s.append(dataSample.getParticleCount());
+      s.append(COMMA);
+      s.append(dataSample.getTemperature());
+      s.append(COMMA);
+      s.append(dataSample.getHumidity());
+      s.append(COMMA);
+      s.append(dataSample.getDownloadTime());
+      write(s.toString());
+      }
+
+   private void write(final String str) throws IOException
+      {
+      writer.write(str);
+      writer.newLine();
+      writer.flush();
+      }
+
+   @Override
+   public void resetStateOfUploadingSamples()
+      {
+      // not supported
+      }
+
+   @Override
+   public void shutdown()
+      {
+      try
+         {
+         writer.close();
+         LOG.debug("CsvDataSampleStore.shutdown(): Successfully shut down the CsvDataSampleStore");
+         }
+      catch (IOException e)
+         {
+         LOG.error("CsvDataSampleStore.shutdown(): IOException while trying to close the writer", e);
+         }
+      }
+   }
