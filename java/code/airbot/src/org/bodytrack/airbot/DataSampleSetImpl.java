3b2c98d92f5f54f054c6a37a6822055aae3aa64d
Chris Bartley <bartley@cmu.edu>
2013-03-27 17:01:38 -0400

    Third-party library upgrades

--- java/code/airbot/src/org/bodytrack/airbot/DataSampleSetImpl.java
+++ java/code/airbot/src/org/bodytrack/airbot/DataSampleSetImpl.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
--- java/code/airbot/src/org/bodytrack/airbot/DatabaseDataSampleStore.java
+++ java/code/airbot/src/org/bodytrack/airbot/DatabaseDataSampleStore.java
@@ -17,7 +17,7 @@ import java.util.TreeSet;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import edu.cmu.ri.createlab.persistence.DatabaseUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
